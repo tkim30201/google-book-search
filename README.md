ce19577b667d77e241cc2e9612ff72f1bbbdace8
tkim30201 <63823948+tkim30201@users.noreply.github.com>
2020-09-29 13:01:00 -0400

    final push

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Google Books React Search
+
+A react-based Google Books Search app.
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/Books.js
+++ src/Books.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
-import SearchArea from './SearchArea'
+import React, { Component } from 'react';
+import SearchArea from './SearchArea';
 import request from 'superagent';
+import BookList from './BookList';
 
 
 class Books extends Component {
@@ -18,7 +19,7 @@ class Books extends Component {
             .get("https://www.googleapis.com/books/v1/volumes")
             .query({ q: this.state.searchField })
             .then((data) => {
-                console.log(data);
+                this.setState({ books: [...data.body.items]})
             })
     }
 
@@ -30,6 +31,7 @@ class Books extends Component {
         return (
             <div>
                 <SearchArea searchBook={this.searchBook} handleSearch={this.handleSearch} />
+                <BookList books={this.state.books} />
             </div>
         );
     }
